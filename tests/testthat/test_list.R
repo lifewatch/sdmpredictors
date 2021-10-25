3dca0f399421c1c5720bda57499e387b270d330e
salvafern <salvador.fernandez@vliz.be>
2021-10-25 17:12:26 +0200

    Remove duplicated layer_code and add test. Fixes #15

--- tests/testthat/test_list.R
+++ tests/testthat/test_list.R
@@ -20,3 +20,13 @@ test_that("list_layers_paleo works", {
   l <- list_layers_paleo()
   expect_gt(nrow(l), 200)
 })
+
+test_that("layer_code is unique", {
+  layer_code <- c(list_layers()$layer_code, 
+                  list_layers_future()$layer_code, 
+                  list_layers_paleo()$layer_code)
+  n_duplications <- anyDuplicated(layer_code)
+  expect_lte(n_duplications, 0)
+  # Explore errors with:
+  # list_layers()[layers$layer_code %in% layers$layer_code[n_duplications], ]
+})
