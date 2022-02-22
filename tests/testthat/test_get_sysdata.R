8ff44c27ddc2213e938ba1930a8c596b33b2c8cb
salvafern <salvador.fernandez@vliz.be>
2022-02-22 18:18:30 +0100

    Update sysdata and add test to check urls http status. Fixes #20

--- tests/testthat/test_get_sysdata.R
+++ tests/testthat/test_get_sysdata.R
@@ -24,3 +24,69 @@ test_that("get_sysdata returns all expected entries", {
   expect_notnull(sysdata$creation)
   expect_gt(length(names(sysdata)), 9)
 })
+
+test_that("all urls exists", {
+  # This test takes a long time. Better to only run interactively
+  skip("Run only interactively")
+  sysdata <- sdmpredictors:::get_sysdata()
+  
+  list_url <- c(sysdata$layerlist$layer_url, 
+                sysdata$layerlistfuture$layer_url,
+                sysdata$layerlistpaleo$layer_url)
+  
+  
+  results <- data.frame(
+    url = NA,
+    error = NA,
+    message = NA
+  )
+  n <- 0
+  total <- length(list_url)
+  
+  # Get the HTTP HEAD of each url and paste into an empty dataframe
+  check_url_status <- function(url){
+    
+    head_url <- httr::HEAD(url)
+    error <- httr::http_error(head_url)
+    messg <- httr::http_status(head_url)$message
+    
+    results <<- rbind(results, c(
+      url, error, messg
+    ))
+
+    n <<- n+1
+
+    message(paste0(n, " out of ", total))
+    
+    Sys.sleep(0.001)
+  }
+  
+  lapply(list_url, check_url_status)
+  
+  failure <- any(as.logical(results$error[-1]))
+  
+  if(failure){
+    message("The following urls failed: ")
+    print(subset(results, as.character(results$error) == TRUE))
+    # write.csv(subset(results, as.character(results$error) == TRUE), "error404.csv")
+  }
+  
+  expect_false(failure)
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
